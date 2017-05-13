6cf7f5ed35089f9c3aaa2c310e25391b07523d0f
Kieran Harper <kieranjharper@gmail.com>
2017-05-13 12:34:34 +1000

    Added test dependencies to swift package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Yakka"
+    name: "Yakka",
+    dependencies: [
+        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1, minor: 1),
+	    .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6, minor: 1)
+    ]
 )
