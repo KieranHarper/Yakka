c20b3db88de94fbb1ab70d737d3c2ce930abceb8
Kieran Harper <kieranjharper@gmail.com>
2018-01-21 20:27:08 +1000

    Upgraded package file to use swift 4 tools

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Yakka"
-
-    // (for running tests)
-    // dependencies: [
-    //     .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
-	//     .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6)
-    // ]
+    name: "Yakka",
+    products: [
+        .library(name: "Yakka", targets: ["Yakka"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(name: "Yakka", path: "Sources"),
+        .testTarget(name: "YakkaTests", dependencies: ["Yakka", "Quick", "Nimble"]),
+    ]
 )
